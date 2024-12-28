b1f03c01296764a53345042c3d2b3f3a3c133eb9
BitNinja01 <bitninja01@pm.me>
2024-12-28 08:48:20 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 setup(
     name='Zazzle',
     packages=['zazzle'],
-    version='0.1.0',
+    version='0.1.1',
     author='BitNinja01',
     author_email='bitninja01@pm.me',
     description='An easy logging library.',
     url='https://github.com/BitNinja01/Zazzle',
-    download_url='https://github.com/BitNinja01/zazzle/archive/refs/tags/0.1.0.tar.gz',
+    download_url='https://github.com/BitNinja01/zazzle/archive/refs/tags/0.1.1.tar.gz',
     license='GNU General Public License v3 (GPLv3)',
     classifiers=[
         'Programming Language :: Python :: 3.10',
